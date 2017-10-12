7cbfa65e5edb7142cfb1a057ea4558a80bd966da
yihdego <yihdego@msn.com>
2017-10-11 21:28:20 -0700

    edited layout and login/user create pages

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -5,6 +5,7 @@ end
 post '/sessions' do
   @user = User.login(params[:email], params[:password])
   if @user
+    session[:user_id] = @user.id
     redirect "/users/#{@user.id}"
   else
     redirect '/sessions/new'
