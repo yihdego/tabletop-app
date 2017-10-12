78a10cf36f9358b2af4f9c9e4a7351d1b4939118
Amanuel <yihdego@msn.com>
2017-10-11 17:44:46 -0700

    writing tags controller logic in edit event route

--- app/controllers/events.rb
+++ app/controllers/events.rb
@@ -12,3 +12,15 @@ get '/events/:id' do
   @event = Event.find(params[:id])
   erb :'/events/show'
 end
+
+put '/events/:id' do
+  @event = Event.find(params[:id])
+  @tag = Tag.find_by(name: params[:tag_name])
+  if @tag = nil
+    @tag = Tag.create(name: params[:tag_name])
+    @event.tags << @tag
+  else
+    @event.tags << @tag
+  end
+  redirect "/events/#{@event.id}"
+end
